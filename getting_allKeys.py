f89c770772602c692a10fb066db7c6947ef027f1
john <johnmugure659@gmail.com>
2023-02-04 01:27:46 +0300

    getting all the keys

--- getting_allKeys.py
+++ getting_allKeys.py
@@ -46,14 +46,5 @@ items = driver.find_elements_by_xpath(
 time.sleep(2)
 print(len(items))
 
-# last_createdItem
-recentItem = items[0]
 
-showKey = recentItem.find_element_by_xpath(
-    "//span[contains(@class,'mb-3 mt-1 v-chip v-chip--clickable v-chip--label v-chip--no-color v-chip--outlined theme--dark v-size--small')]"
-).click()
-
-keys = recentItem.find_element_by_xpath(
-    "//span[contains(@class,'text-truncate ellipsize-key ellipsize-keyAndSecret-Undo')]"
-).text
-print(keys)
+# looping all items for the keys
